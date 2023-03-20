4030fca0859040d447facc749b4c598cc93ddae3
stef <zhangcheng0325@outlook.com>
2023-03-20 09:34:22 +0800

    commit weather application prototype

---
+++ app/src/main/java/cn/edu/snnu/zc/myweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.snnu.zc.myweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
