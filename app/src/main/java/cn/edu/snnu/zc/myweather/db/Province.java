e15e10e9a10db97d07b5beff2ad2030c3930f0da
stef <zhangcheng0325@outlook.com>
2023-03-20 12:03:52 +0800

    add the entity and confirgure the litepal

---
+++ app/src/main/java/cn/edu/snnu/zc/myweather/db/Province.java
@@ -0,0 +1,23 @@
+package cn.edu.snnu.zc.myweather.db;
+
+public class Province {
+    private int id;
+
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
